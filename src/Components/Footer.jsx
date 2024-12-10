1b7437f70363c0863e0185dc60a1460732c677a2
Mohamed Sano <150654331+Sanomohamed@users.noreply.github.com>
2024-12-10 15:47:44 +0000

    comit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,4 @@
-/**
- * Footer component
- *
- * Displays avenues to contact you.
- * Contact information is passed in from the App component that
- * renders the Footer.
- *
- * If a social value has an empty string it will not be displayed.
- */
+
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -19,12 +11,6 @@ import mediumIcon from "../images/socials/medium.svg";
 import twitterIcon from "../images/socials/twitter.svg";
 import youTubeIcon from "../images/socials/youtube.svg";
 
-/**
- * 💡 Learning resources
- *
- *  HTML hyperlinks: https://www.w3schools.com/html/html_links.asp
- *  Opening links in new tabs: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
- */
 
 const Footer = (props) => {
   const {
